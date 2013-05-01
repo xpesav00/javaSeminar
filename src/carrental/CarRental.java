6431416d03d9a77db45b827d8831d698726697ad
honzap <honzap@honzap-PC>
2013-05-01 15:09:35 +0200

    repair bugs in mainWindow, create start app with gui

--- src/carrental/CarRental.java
+++ src/carrental/CarRental.java
@@ -9,6 +9,7 @@
  */
 package carrental;
 
+import common.MainWindow;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -20,39 +21,25 @@ import java.util.logging.Logger;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
+import javax.swing.JFrame;
 import org.apache.commons.dbcp.BasicDataSource; 
 
 public class CarRental {
 
+	private static void createAndShowGUI() {
+		JFrame mainWindow = new MainWindow();
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindow.setVisible(true);
+	}
+	
 	/**
 	 * @param args the command line arguments
 	 */
-	public static void main(String[] args) throws NamingException{
-		try {
-			//connect to db
-			//Connection connection = DriverManager.getConnection("jdbc:derby://localhost:1527/javaSeminar", "developer", "developer");
-			
-			BasicDataSource dataSource = new BasicDataSource();
-			dataSource.setDriverClassName("org.apache.derby.jdbc.Driver169");
-			dataSource.setUrl("jdbc:derby://localhost:1527/javaSeminar");
-			//dataSource.setUrl("jdbc:derby:memory:javaSeminar;create=true");
-			dataSource.setUsername("developer");
-			dataSource.setPassword("developer");
-			Connection connection = dataSource.getConnection();
-			
-                        
-			//here is possible to create  queries
-			/*PreparedStatement ps = connection.prepareStatement("SELECT * from car");
-			ResultSet rs = ps.executeQuery();
-			
-			while(rs.next()) {
-				System.out.println(rs.getString("name"));
-			}*/
-			
-			
-		} catch (SQLException ex) {
-			Logger.getLogger(CarRental.class.getName()).log(Level.SEVERE, null, ex);
-		} 
-		
+	public static void main(String[] args) throws NamingException{		
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+			    createAndShowGUI();
+			}
+		});
 	}
 }
